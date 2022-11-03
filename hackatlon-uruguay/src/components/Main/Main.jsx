38daf862248476c43d4267c398b6281d777607b6
facundovacanicencosud <111792420+facundovacanicencosud@users.noreply.github.com>
2022-11-03 13:06:15 -0300

    add filter search

--- hackatlon-uruguay/src/components/Main/Main.jsx
+++ hackatlon-uruguay/src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ListProducts from '../ListProducts/ListProducts';
-import { getProducts } from '../../utils/apiConfig';
+import { getProducts, getProductsFilter } from '../../utils/apiConfig';
 import FilterOptions from '../FilterOptions/FilterOptions';
 
 const Main = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState("");
+    const [query, setQuery] = useState("");
+
+    const inputRef = useRef();
     useEffect(()=>{
         (async()=>{
             const res = await getProducts();
@@ -13,9 +17,34 @@ const Main = () => {
                 setProducts(res);
                 setLoading(false); 
             }, 1000);
-            console.log("main");
         })();
     },[]);
+
+
+    useEffect(()=>{
+        if(order === "menorPrecio"){
+            let productosOrder = [...products];
+            productosOrder.sort((prod1, prod2)=> prod1.price-prod2.price);
+            setProducts(productosOrder);
+        }else if(order === "mayorPrecio"){
+            let productosOrder = [...products];
+            productosOrder.sort((prod1, prod2)=> prod2.price-prod1.price);
+            setProducts(productosOrder);
+        }
+    },[order]);
+
+    useEffect(()=>{
+        (async()=>{
+            const res = await getProductsFilter(query);
+            setProducts(res)
+        })();
+    },[query]);
+
+    const cambio = ({target}) => {
+        setQuery(target.value)
+    }
+
+
     return (
         <main>
             <section>
@@ -24,8 +53,10 @@ const Main = () => {
                     <div className='loading'>Cargando...</div>
                 : products.length > 0 ? 
                     <>
-                        <FilterOptions />
+                        <FilterOptions order={order} setOrder={setOrder}/>
                         <ListProducts products={products} />
+                        <input onChange={(e)=> cambio(e)} ref={inputRef} type="text" />
+                        <button>Click para filtrar</button>
                     </>
                 : <div>No hay Productos</div>
                 }
