26f0d6627d1a75cdd5e8633f997fe924d05649ae
Marcos Viera <marcos.cortazar@cencosud.com>
2022-11-03 13:05:19 -0300

    trabajando sobre header

--- hackatlon-uruguay/src/components/Header/Header.jsx
+++ hackatlon-uruguay/src/components/Header/Header.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import menuImage from './../../assets/menu.svg'
+import React from "react";
+import menuImage from "./../../assets/menu.svg";
+import logoCenco from "./../../assets/logoCenco.png";
+
+import "./Header.css";
+import {Link, Links} from 'react-router-dom'
 
 const Header = () => {
-
   return (
-    <nav>
-        <div>
-        <img
+    <header>
+      
+        <div className="header__brand-image-container">
+          <img
             src={menuImage}
             alt="Menú hamburguesa"
             className="header__hamburguer-menu-img"
           />
         </div>
-    </nav>
-  )
-}
 
-export default Header
+        <div className="header__image-container">
+          <img
+            src={menuImage}
+            alt="Menú hamburguesa"
+            className="header__hamburguer-menu-img"
+          />
+        </div>
+
+        <div className="header__input-search">
+          <input
+            type="text"
+            placeholder="Buscar productos..."
+          />
+        </div>
+
+        <nav className="list-items">
+          <ul>
+            <Link to="/login"><li>Inciar sesión</li></Link>
+            {/* <Link ><li>Listado de products</li></Link> */}
+          </ul>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
--- hackatlon-uruguay/src/views/Producto.jsx
+++ hackatlon-uruguay/src/views/Producto.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import imagenMuestra from "./../assets/imagen-muestra.jpeg";
-import './Producto.css';
-import {useParams} from 'react-router-dom'
+import "./Producto.css";
+import { useParams } from "react-router-dom";
 
 const Producto = (props) => {
-
-    let productId = useParams().id;
-console.log(productId);
-
-
+  let productId = useParams().id;
+  console.log(productId);
 
   return (
     <section className="main-product-card">
       <article className="">
         <div className="main-product-title">
-        <h2>Título del producto</h2>
+          <h2>Título del producto</h2>
         </div>
         <div className="main-product-card__image">
           <img src={imagenMuestra} />
         </div>
-        <div className="main-product-card__price">
-          <h3>Precio: $596</h3>
-          <h3>Tienda: Amazon</h3>
-        </div>
-        <div className="main-product-card__description">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nobis,
-            facilis ipsam saepe corrupti tempora cum nesciunt quia temporibus?
-            Consequatur cupiditate, esse corrupti explicabo, nulla vel vitae
-            reprehenderit molestias, corporis aperiam commodi maxime nemo
-            asperiores quibusdam harum iusto assumenda ab.
-          </p>
+        <div className="main-product-card__all-content">
+          <div className="main-product-card__info">
+            <div className="main-product-title__alternative">
+              <h2>Título del producto</h2>
+            </div>
+            <div className="main-product-card__price">
+              <h3>Precio: $596</h3>
+              <h3>Tienda: Amazon</h3>
+            </div>
+            <div className="main-product-card__description">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+                nobis, facilis ipsam saepe corrupti tempora cum nesciunt quia
+                temporibus? Consequatur cupiditate, esse corrupti explicabo,
+                nulla vel vitae reprehenderit molestias, corporis aperiam
+                commodi maxime nemo asperiores quibusdam harum iusto assumenda
+                ab.
+              </p>
+            </div>
+          </div>
+          <div className="main-product-card__image-alternative">
+            <img src={imagenMuestra} />
+          </div>
         </div>
       </article>
     </section>
